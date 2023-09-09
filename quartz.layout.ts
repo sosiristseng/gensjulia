f1decb0fe93a28cd3faffd125d7f538eaa192149
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-09-09 15:20:22 +0800

    link to my pages

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/sosiristseng/gensjulia",
-      "Main site": "https://wwtseng.pages.dev"
+      "WenWei Tseng": "https://wwtseng.pages.dev"
     },
   }),
 }
